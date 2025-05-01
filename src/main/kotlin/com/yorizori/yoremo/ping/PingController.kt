430fed95c37eb10f1f58923012c7a246d974811b
woodying <wkyungjin1103@gmail.com>
2025-05-01 11:07:25 +0900

    feat: project init

---
+++ src/main/kotlin/com/yorizori/yoremo/ping/PingController.kt
@@ -0,0 +1,17 @@
+package com.yorizori.yoremo.ping
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PingController {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @GetMapping("/ping")
+    fun ping(): String {
+        logger.info("ping request received. thread: ${Thread.currentThread()}")
+        return "pong"
+    }
+}
