7d8b3aa6b4d165ebf3f72c082427fd0024ad9687
woodying <wkyungjin1103@gmail.com>
2025-05-01 11:57:14 +0900

    feat: BaseEntity 생성 및 Auditing config 추가

---
+++ src/main/kotlin/com/yorizori/yoremo/adapter/postgresql/common/BaseEntity.kt
@@ -0,0 +1,23 @@
+package com.yorizori.yoremo.adapter.postgresql.common
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.Instant
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at")
+    lateinit var createdAt: Instant
+        protected set
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    lateinit var updatedAt: Instant
+        protected set
+}
---
+++ src/main/kotlin/com/yorizori/yoremo/adapter/postgresql/common/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package com.yorizori.yoremo.adapter.postgresql.common
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig
