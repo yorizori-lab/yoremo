83b67223f8c196cf49902de8f6d0cfa25006adfc
woodying <wkyungjin1103@gmail.com>
2025-05-02 09:14:38 +0900

    build: spring AI 및 open AI 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,18 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.ai:spring-ai-starter-model-openai")
+
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.ai:spring-ai-bom:1.0.0-M8")
+	}
 }
 
 kotlin {
