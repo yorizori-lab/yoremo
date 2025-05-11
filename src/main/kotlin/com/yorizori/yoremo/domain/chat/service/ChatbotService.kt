49c0f7d25b35d2967a1923060bdb5f4b03f48290
woodying <wkyungjin1103@gmail.com>
2025-05-11 16:33:44 +0900

    feat: chat context 기능 추가 (in-memory)

--- src/main/kotlin/com/yorizori/yoremo/domain/chat/service/ChatbotService.kt
+++ src/main/kotlin/com/yorizori/yoremo/domain/chat/service/ChatbotService.kt
@@ -2,24 +2,40 @@ package com.yorizori.yoremo.domain.chat.service
 
 import com.yorizori.yoremo.adapter.`in`.web.chat.message.ChatMessage
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor
+import org.springframework.ai.chat.memory.ChatMemory
 import org.springframework.ai.chat.model.ChatModel
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.stereotype.Service
 
+
 @Service
 class ChatbotService(
-    private val vectorStore: VectorStore,
-    private val chatModel: ChatModel
+    vectorStore: VectorStore,
+    chatModel: ChatModel,
+    chatMemory: ChatMemory,
 ) {
 
-    fun getResponse(request: ChatMessage.Request): ChatMessage.Response {
-        val qaAdvisor = QuestionAnswerAdvisor.builder(vectorStore).build()
+    private val chatClient = ChatClient.builder(chatModel)
+        .defaultAdvisors(
+            QuestionAnswerAdvisor.builder(vectorStore).build(),
+            MessageChatMemoryAdvisor.builder(chatMemory).build()
+        )
+        .build()
 
-        val response = ChatClient.builder(chatModel).build()
-            .prompt()
-            .advisors(qaAdvisor)
+    fun getResponse(request: ChatMessage.Request): ChatMessage.Response {
+        val response = chatClient.prompt()
             .user(request.question)
+            .advisors { advisor ->
+                if (request.sessionId != null) {
+                    advisor.param(
+                        AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY,
+                        request.sessionId
+                    )
+                }
+            }
             .call()
             .content()
 
