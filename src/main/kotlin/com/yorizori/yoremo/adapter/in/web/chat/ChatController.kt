56e30fb7d66613c5022deae5dfae01c16c9e1ba9
woodying <wkyungjin1103@gmail.com>
2025-05-14 21:00:00 +0900

    feat: 챗 메시지 API path 변경

--- src/main/kotlin/com/yorizori/yoremo/adapter/in/web/chat/ChatController.kt
+++ src/main/kotlin/com/yorizori/yoremo/adapter/in/web/chat/ChatController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/chat")
+@RequestMapping("/api/chat/v1")
 class ChatController(
     private val chatbotService: ChatbotService
 ) {
