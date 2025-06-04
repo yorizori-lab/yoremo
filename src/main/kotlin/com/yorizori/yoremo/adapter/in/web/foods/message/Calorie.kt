bad4db7352714520fe7d00044478288be21dc9c1
cjkim <chanjin5212@gmail.com>
2025-06-04 09:47:27 +0900

    feat: 칼로리 자동 계산 ai 서비스 추가

---
+++ src/main/kotlin/com/yorizori/yoremo/adapter/in/web/foods/message/Calorie.kt
@@ -0,0 +1,12 @@
+package com.yorizori.yoremo.adapter.`in`.web.foods.message
+
+abstract class Calorie {
+    data class Request(
+                       val title: String,
+                       val ingredients: List<String>
+    )
+
+    data class Response(
+        val caloriesPer100g: Int
+    )
+}
