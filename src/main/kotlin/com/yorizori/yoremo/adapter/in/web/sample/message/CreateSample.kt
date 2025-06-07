2aeb290d99ffa56bb05272bc678b65df0f8de322
woodying <wkyungjin1103@gmail.com>
2025-06-07 18:31:42 +0900

    chore: sample 코드 제거

--- src/main/kotlin/com/yorizori/yoremo/adapter/in/web/sample/message/CreateSample.kt
+++
@@ -1,8 +0,0 @@
-package com.yorizori.yoremo.adapter.`in`.web.sample.message
-
-abstract class CreateSample {
-
-    data class Request(val message: String)
-
-    data class Response(val id: Long)
-}
